d54fca556cc1922e7bfa374958a62f76aff49795
Seth Lewis <97172419+slandonlewis@users.noreply.github.com>
2022-08-18 11:33:39 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿// using System;
+
+// namespace Intro
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine("Hello World!");
+//         }
+//     }
+// }
+
+// System.Console.Write("Who would you like to say hello to? ");
+
+// string name = System.Console.ReadLine();
+
+// if (string.IsNullOrWhiteSpace(name))
+// {
+//     System.Console.WriteLine("Fine, don't say 'hello'!");
+// }
+// else
+// {
+//     System.Console.WriteLine($"Hello, {name}!");
+// }
+
+using System;
+
+Console.Write("Who would you like to say hello to? ");
+
+string name = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(name))
+{
+    Console.WriteLine("Fine, don't say 'hello'!");
+}
+else
+{
+    Console.WriteLine($"Hello, {name}!");
+}
